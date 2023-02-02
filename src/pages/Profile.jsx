8f0fc51ce60c2f641f2607d811d7e37516deb298
alireza kargar <imanesche@gmail.com>
2023-02-03 00:16:34 +0330

    fetch list of my connection

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { AddIcon, CheckIcon, CloseIcon } from '@chakra-ui/icons'
 import {
   Table,
   Thead,
@@ -9,47 +10,65 @@ import {
   TableCaption,
   TableContainer,
   Container,
-  Card, CardHeader, CardBody, CardFooter, Stack, Button
+  Card, CardHeader, CardBody, CardFooter, Stack, Button, ListIcon
 } from '@chakra-ui/react'
+import axios from 'axios'
+import { Component } from 'react'
 
-export default function Profile() {
-  return (
-    <Card>
-      <CardHeader>
-        <Stack spacing={4} direction='row' align='center'>
-          <Button colorScheme='linkedin' size='sm'>Export My Connections Data</Button>
-        </Stack>
-      </CardHeader>
-      <CardBody>
-        <TableContainer>
-          <Table variant='striped'>
-            <Thead>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td isNumeric>25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-                <Td isNumeric>30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-                <Td isNumeric>0.91444</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </CardBody>
-    </Card>
-  )
+
+export default class Profile extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`http://localhost:3333/api/connections/my_connection/1`)
+      .then(({ data }) => {
+        this.setState({
+          data: data
+        })
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardHeader>
+          <Stack spacing={4} direction='row' align='center'>
+            <Button colorScheme='linkedin' size='sm'>Export My Connections Data</Button>
+          </Stack>
+        </CardHeader>
+        <CardBody>
+          <TableContainer>
+            <Table variant='striped'>
+              <Thead>
+                <Tr>
+                  <Th>name</Th>
+                  <Th>is From MyConnection?</Th>
+                  <Th>exported Connection Data?</Th>
+                  <Th>exported section Data?</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {this.state.data.map((item, index) => {
+                  return (
+                    <Tr key={index}>
+                      <Td>{item.name}</Td>
+                      <Td>{(item.isFromMyConnection) ? <AddIcon as={CheckIcon} /> : <AddIcon as={CloseIcon} />}</Td>
+                      <Td>{(item.exportedConnectionData) ? <AddIcon as={CheckIcon} /> : <AddIcon as={CloseIcon} />}</Td>
+                      <Td>{(item.exportedSectionsData) ? <AddIcon as={CheckIcon} /> : <AddIcon as={CloseIcon} />}</Td>
+                    </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </CardBody>
+      </Card>
+    )
+  }
+
 }
